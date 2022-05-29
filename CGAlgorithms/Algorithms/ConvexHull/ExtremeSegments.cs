0bd0c9dd17b605e83b33e18de0031c5dac983a16
AbodAly <abdullah2018170546@cis.asu.edu.eg>
2022-05-29 21:38:26 +0200

    Graham scan imporvment

--- CGAlgorithms/Algorithms/ConvexHull/ExtremeSegments.cs
+++ CGAlgorithms/Algorithms/ConvexHull/ExtremeSegments.cs
@@ -9,45 +9,122 @@ namespace CGAlgorithms.Algorithms.ConvexHull
 {
     public class ExtremeSegments : Algorithm
     {
-        private bool valid(int i, int j, int k)
-        {
-            HashSet<int> s = new HashSet<int>();
-            s.Add(i);
-            s.Add(j);
-            s.Add(k);
-            return s.Count == 3;
-        }
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
-           
-            if (points.Count < 4)
+            bool[] Visted = new bool[points.Count];
+            for (int i = 0; i < points.Count; i++)
             {
-                outPoints = new List<Point>(points);
-                return;
+                Visted[i] = false;
             }
-            HashSet<Point> setPoints = new HashSet<Point>();
-            for (int i = 0; i < points.Count; ++i)
-                for (int j = i + 1; j < points.Count; ++j)
+            points.Sort(Compare);
+            List<Point> UniqList = new List<Point>();
+            UniqList.Add(points[0]);
+            for (int i = 1; i < points.Count; i++)
+            {
+                if (points[i].Equals(points[i - 1]))
                 {
-                    Enums.TurnType turn = HelperMethods.CheckTurn(new Line(points[i], points[j]), points[0]);
-                    bool ok = true;
-                    for (int k = 0; k < points.Count; ++k)
-                        if (valid(i, j, k))
+                    continue;
+                }
+                UniqList.Add(points[i]);
+            }
+            points = UniqList;
+            for (int i = 0; i < points.Count; i++)
+            {
+                for (int j = 0; j < points.Count; j++)
+                {
+                    if (points[i].Equals(points[j])) continue;
+
+                    Line l = new Line(points[i], points[j]);
+                    int left = 0, right = 0, col = 0;
+                    for (int k = 0; k < points.Count; k++)
+                    {
+                        if (points[i].Equals(points[k]) || points[k].Equals(points[j]))
+                            continue;
+                        if (HelperMethods.CheckTurn(l, points[k]) == Enums.TurnType.Left) left++;
+                        else if (HelperMethods.CheckTurn(l, points[k]) == Enums.TurnType.Right) right++;
+                        else if (HelperMethods.CheckTurn(l, points[k]) == Enums.TurnType.Colinear &&
+                                points[k].X >= l.Start.X && points[k].X <= l.End.X &&
+                                points[k].Y >= l.Start.Y && points[k].Y <= l.End.Y)
+                            col++;
+                    }
+                    if (left == points.Count - col - 2 || right == points.Count - col - 2)
+                    {
+                        outLines.Add(l);
+                        for (int ii = 0; ii < outPoints.Count; ii++)
                         {
-                            Enums.TurnType curTurn = HelperMethods.CheckTurn(new Line(points[i], points[j]), points[k]);
-                            turn = (turn == Enums.TurnType.Colinear) ? curTurn : turn;
-                            if (curTurn != Enums.TurnType.Colinear && turn != curTurn)
-                            { ok = false; break; }
-                            else if (curTurn == Enums.TurnType.Colinear && !HelperMethods.PointOnLine(points[k], points[i], points[j]))
-                            { ok = false; break; }
+                            if (points[i].X == outPoints[ii].X && points[i].Y == outPoints[ii].Y)
+                            {
+                                Visted[i] = true;
+                                break;
+                            }
+                        }
+                        if (Visted[i] == false)
+                        {
+                            outPoints.Add(points[i]);
+                            Visted[i] = true;
+                        }
+                        if (Visted[j] == false)
+                        {
+                            outPoints.Add(points[j]);
+                            Visted[j] = true;
+                        }
+                    }
+                }
+            }
+            if (outPoints.Count == 0 && points.Count > 0)
+            {
+                for (int i = 0; i < points.Count; i++)
+                {
+                    for (int j = 0; j < points.Count; j++)
+                    {
+                        if (points[i].Equals(points[j])) continue;
+                        Line l = new Line(points[i], points[j]);
+                        int col = 0;
+                        for (int k = 0; k < points.Count; k++)
+                        {
+                            if (j == k || k == i)
+                                continue;
+                            if (HelperMethods.CheckTurn(l, points[k]) == Enums.TurnType.Colinear &&
+                                points[k].X >= l.Start.X && points[k].X <= l.End.X &&
+                                points[k].Y >= l.Start.Y && points[k].Y <= l.End.Y)
+                            {
+                                col++;
+                            }
+                        }
+                        if (col == points.Count - 2)
+                        {
+                            outLines.Add(l);
+                            for (int ii = 0; ii < outPoints.Count; ii++)
+                            {
+                                if (points[i].X == outPoints[ii].X && points[i].Y == outPoints[ii].Y)
+                                {
+                                    Visted[i] = true;
+                                    break;
+                                }
+                            }
+                            if (Visted[i] == true)
+                                continue;
+                            outPoints.Add(points[i]);
+                            outPoints.Add(points[j]);
+                            Visted[i] = true;
+                            Visted[j] = true;
                         }
 
-                    if (ok)
-                    { setPoints.Add(points[i]); setPoints.Add(points[j]); }
-                }
-            outPoints = new List<Point>(setPoints);
-        }
 
+                    }
+
+                }
+            }
+
+
+            if (outPoints.Count == 0 && points.Count > 0)
+                outPoints.Add(points[0]);
+        }
+        static int Compare(Point a, Point b)
+        {
+            if (a.X == b.X) return a.Y.CompareTo(b.Y);
+            return a.X.CompareTo(b.X);
+        }
         public override string ToString()
         {
             return "Convex Hull - Extreme Segments";
--- CGAlgorithms/Algorithms/ConvexHull/GrahamScan.cs
+++ CGAlgorithms/Algorithms/ConvexHull/GrahamScan.cs
@@ -9,109 +9,111 @@ namespace CGAlgorithms.Algorithms.ConvexHull
 {
     public class GrahamScan : Algorithm
     {
-        public static double CalcAngle(Point p1, Point p2, Point p3)
-        {
-            double angle = 0.0;
-            Point a = new Point(0.0, 0.0);
-            Point b = new Point(0.0, 0.0);
-
-            Line l1 = new Line(p1, p2);
-            Line l2 = new Line(p1, p3);
-
-            a = HelperMethods.GetVector(l1);
-            b = HelperMethods.GetVector(l2);
-            double dotProduct = (a.X * b.X + a.Y * b.Y);
-            double crossProduct = HelperMethods.CrossProduct(a, b);
-
-            angle = Math.Atan2(dotProduct, crossProduct) * 180 / Math.PI;
-
-            return angle;
-        }
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
-            points = points.Distinct().ToList();
 
-            if (points.Count == 1)
+            points.Sort(compareXY);
+            List<Point> uniqPoints = new List<Point>();
+            uniqPoints.Add(points[0]);
+            for (int i = 1; i < points.Count; i++)
             {
-                outPoints.Add(points[0]);
+                if (points[i].Equals(points[i - 1]))
+                {
+                    continue;
+                }
+                uniqPoints.Add(points[i]);
             }
-            else
+            points = uniqPoints;
+            
+
+            double MinimY = double.MaxValue;
+            double x = 0;
+            bool[] vis = new bool[points.Count];
+            for (int i = 0; i < points.Count; i++)
+                vis[i] = false;
+            int ind = 0;
+            for (int i = 0; i < points.Count; i++)
             {
-                double minY = 100000;
-                int index = -1;
-                for (int i = 0; i < points.Count; i++)
+                if (MinimY > points[i].Y)
                 {
-                    if (points[i].Y < minY)
-                    {
-                        index = i;
-                        minY = points[i].Y;
-                    }
-                }
-                Point minPoint = points[index];
-                double x = minPoint.X + 100000000;
-                double y = minPoint.Y;
-                Point virtualPoint = new Point(x, y);
-                points.RemoveAt(index);
-                List<KeyValuePair<double, Point>> sortedAngles = new List<KeyValuePair<double, Point>>();
-                for (int i = 0; i < points.Count; i++)
-                {
-
-                    double angle = CalcAngle(minPoint, virtualPoint, points[i]);
-
-                    sortedAngles.Add(new KeyValuePair<double, Point>(angle, points[i]));
-                }
-
-                sortedAngles.Sort((s, ss) => s.Key.CompareTo(ss.Key));
-                Stack<Point> myStack = new Stack<Point>();
-                myStack.Push(minPoint);
-                myStack.Push(sortedAngles[0].Value);
-
-                for (int i = 1; i < points.Count; i++)
-                {
-                    Point p1 = myStack.Pop();
-                    Point p2 = myStack.Pop();
-                    myStack.Push(p2);
-                    myStack.Push(p1);
-                    Line l = new Line(p1, p2);
-                    Enums.TurnType t = HelperMethods.CheckTurn(l, sortedAngles[i].Value);
-
-                    while (myStack.Count > 2 && t != Enums.TurnType.Left)
-                    {
-                        myStack.Pop();
-
-                        p1 = myStack.Pop();
-                        p2 = myStack.Pop();
-
-                        myStack.Push(p2);
-                        myStack.Push(p1);
-                        l = new Line(p1, p2);
-                        t = HelperMethods.CheckTurn(l, sortedAngles[i].Value);
-
-                    }
-                    if (t == Enums.TurnType.Colinear)
-                    {
-                        double d1 = Math.Sqrt(Math.Pow((sortedAngles[i].Value.X - p1.X), 2) + Math.Pow((sortedAngles[i].Value.Y - p1.Y), 2));
-                        double d2 = Math.Sqrt(Math.Pow((p2.X - p1.X), 2) + Math.Pow((p2.Y - p1.Y), 2));
-                        if (d1 > d2)
-                        {
-                            myStack.Pop();
-                        }
-                    }
-                    myStack.Push(sortedAngles[i].Value);
-                }
-
-
-                while (myStack.Count != 0)
-                {
-                    outPoints.Add(myStack.Pop());
+                    MinimY = points[i].Y;
+                    x = points[i].X;
+                    ind = i;
                 }
             }
+            Line MakeLIne = new Line(new Point(x, MinimY), new Point(x + 1000.0, MinimY));
 
+            List<KeyValuePair<double, int>> list = new List<KeyValuePair<double, int>>();
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (i == ind) continue;
+                Point v1 = PointTOVector(MakeLIne.Start, MakeLIne.End);
+                Point v2 = PointTOVector(MakeLIne.Start, points[i]);
+                double cros = HelperMethods.CrossProduct(v1, v2);
+                double dot = HelperMethods.dot(v1, v2);
+                
+                double angle = Math.Atan2(cros, dot) * (180.00 / Math.PI); ;
+                if (angle < 0)
+                    angle += 360;
 
+                list.Add(new KeyValuePair<double, int>(angle, i));
+            }
+            list.Sort(Compare1);
+
+                     Stack<int> st = new Stack<int>();
+            st.Push(ind);
+            if (list.Count > 0)
+                st.Push(list[0].Value);
+            for (int i = 1; i < points.Count - 1 && st.Count >= 2; i++)
+            {
+                int pp1 = st.Peek();
+                Point p1 = points[pp1];
+                st.Pop();
+                int pp2 = st.Peek();
+                Point p2 = points[pp2];
+
+                st.Push(pp1);
+
+                MakeLIne = new Line(p2, p1);
+                if (HelperMethods.CheckTurn(MakeLIne, points[list[i].Value]) == Enums.TurnType.Left)
+                {
+                    st.Push(list[i].Value);
+                }
+                else if (HelperMethods.CheckTurn(MakeLIne, points[list[i].Value]) == Enums.TurnType.Colinear)
+                {
+                    st.Pop();
+                    st.Push(list[i].Value);
+                }
+                else
+                {
+                    st.Pop();
+                    i--;
+                }
+            }
+            while (st.Count > 0)
+            {
+                outPoints.Add(points[st.Peek()]);
+                st.Pop();
+            }
+        }
+
+        Point PointTOVector(Point a, Point b)
+        {
+            return new Point(b.X - a.X, b.Y - a.Y);
+        }
+        static int compareXY(Point a, Point b)
+        {
+            if (a.X == b.X) return a.Y.CompareTo(b.Y);
+            return a.X.CompareTo(b.X);
         }
         public override string ToString()
         {
             return "Convex Hull - Graham Scan";
+        }
+        static int Compare1(KeyValuePair<double, int> a, KeyValuePair<double, int> b)
+        {
+            if (a.Key == b.Key) return a.Value.CompareTo(b.Value);
+            return a.Key.CompareTo(b.Key);
         }
     }
 }
