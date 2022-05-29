3a0d1e13d0f07ee08faf56a1c7f40ff4f6e7ba73
AbodAly <abdullah2018170546@cis.asu.edu.eg>
2022-05-29 21:46:58 +0200

    Added Motone Partion

--- CGAlgorithms/Algorithms/PolygonTriangulation/MonotoneTriangulation.cs
+++ CGAlgorithms/Algorithms/PolygonTriangulation/MonotoneTriangulation.cs
@@ -4,106 +4,250 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CGUtilities;
+using CGUtilities.DataStructures;
 
 namespace CGAlgorithms.Algorithms.PolygonTriangulation
 {
-    class MonotoneTriangulation : Algorithm
+    class MonotonePartitioning : Algorithm
     {
-        public class ChainPoint
+        public class MonoPoint
         {
-            public Point p;
-            public int chain;
-            public ChainPoint(Point _p, int _chain)
-            { p = _p; chain = _chain; }
+            public MonoPoint(Point _p, int _i) { i = _i; p = _p; }
+            public Point p; public int i;
         }
+        List<Point> p;
+        List<MonoPoint> Q;
+        OrderedSet<int> T;
+        List<int> helper;
+        List<List<int>> neighbors;
+        List<Line> ll;
         public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
             if (polygons.Count == 0)
                 return;
-            List<Point> p = new List<Point>();
+            ll = outLines;
+            helper = new List<int>(); neighbors = new List<List<int>>();
+            p = new List<Point>(); Q = new List<MonoPoint>(); T = new OrderedSet<int>(TCompX);
             for (int i = 0; i < polygons[0].lines.Count; ++i)
-                p.Add(polygons[0].lines[i].Start);
+            { p.Add(polygons[0].lines[i].Start); neighbors.Add(new List<int>()); helper.Add(-1); }
             checkPolygon(p);
-            List<ChainPoint> cp = checkMonotone(p);
-            if (cp == null)
-                return;
-            cp = sort(cp);
-            Stack<ChainPoint> stk = new Stack<ChainPoint>();
-            stk.Push(cp[0]);
-            stk.Push(cp[1]);
-            for (int i = 2; i < p.Count - 1;)
+            for (int i = 0; i < polygons[0].lines.Count; ++i)
+                Q.Add(new MonoPoint(polygons[0].lines[i].Start, i));
+            Q.Sort(comp);
+            for (int i = 0; i < Q.Count; ++i)
             {
-                ChainPoint top = stk.Peek();
-                if (cp[i].chain == top.chain)
+                switch (checkCase(Q[i].i))
                 {
-                    stk.Pop();
-                    ChainPoint top2 = stk.Peek();
-                    if ((HelperMethods.CheckTurn(new Line(top2.p, top.p), cp[i].p) == Enums.TurnType.Right && top.chain == 1) ||
-                        (HelperMethods.CheckTurn(new Line(top2.p, top.p), cp[i].p) == Enums.TurnType.Left && top.chain == -1))
-                        outLines.Add(new Line(cp[i].p, top2.p));
-                    else
-                    {
-                        stk.Push(top);
-                        stk.Push(cp[i]);
-                        i++;
-                    }
+                    case 0:
+                        handleStart(Q[i].i);
+                        break;
+                    case 1:
+                        handleEnd(Q[i].i);
+                        break;
+                    case 2:
+                        handleSplit(Q[i].i);
+                        break;
+                    case 3:
+                        handleMerge(Q[i].i);
+                        break;
+                    case 4:
+                        handleRegular(Q[i].i);
+                        break;
                 }
-                else
+            }
+
+            //triangulateSubPol();
+            //for(int i=0;i<ll.Count;i++)
+            //    outLines.Add(
+        }
+
+        private void handleStart(int i)
+        {
+            T.Add(i);
+            helper[i] = i;
+        }
+        private void handleEnd(int i)
+        {
+            int prevE = (i + p.Count - 1) % p.Count;
+            if (checkCase(helper[prevE]) == 3)
+            {
+                ll.Add(new Line(p[helper[prevE]], p[i]));
+                neighbors[Math.Max(helper[prevE], i)].Add(Math.Min(helper[prevE], i));
+                //neighbors[helper[prevE]].Add(i);
+                //neighbors[i].Add(helper[prevE]);
+            }
+            T.Remove(prevE);
+        }
+        private void handleRegular(int i)
+        {
+            if (interiorRight(i))
+            {
+                handleEnd(i);
+                T.Add(i);
+                helper[i] = i;
+            }
+            else
+            {
+                int dirLeft = T.DirectUpperAndLower(i).Value;
+                if (checkCase(helper[dirLeft]) == 3)
                 {
-                    while (stk.Count != 1)
-                    {
-                        ChainPoint top2 = stk.Pop();
-                        outLines.Add(new Line(cp[i].p, top2.p));
-                    }
-                    stk.Pop();
-                    stk.Push(top);
-                    stk.Push(cp[i]);
-                    i++;
+                    ll.Add(new Line(p[helper[dirLeft]], p[i]));
+                    neighbors[Math.Max(helper[dirLeft], i)].Add(Math.Min(helper[dirLeft], i));
+                    //neighbors[helper[dirLeft]].Add(i);
+                    //neighbors[i].Add(helper[dirLeft]);
                 }
+                helper[dirLeft] = i;
 
             }
         }
 
-        private List<ChainPoint> sort(List<ChainPoint> cp)
+        private void handleMerge(int i)
         {
-            List<ChainPoint> res = new List<ChainPoint>();
-            res.Add(cp[0]);
-            int left = 1;
-            int right = cp.Count - 1;
-            while (cp[left].chain != 0 || cp[right].chain != 0)
+            handleEnd(i);
+            int dirLeft = T.DirectUpperAndLower(i).Value;
+            if (checkCase(helper[dirLeft]) == 3)
             {
-                if (cp[left].p.Y > cp[right].p.Y)
-                { res.Add(cp[left]); left = (left + 1) % cp.Count; }
-                else
-                { res.Add(cp[right]); right = (right + cp.Count - 1) % cp.Count; }
+                ll.Add(new Line(p[helper[dirLeft]], p[i]));
+                neighbors[Math.Max(helper[dirLeft], i)].Add(Math.Min(helper[dirLeft], i));
+                //neighbors[helper[dirLeft]].Add(i);
+                //neighbors[i].Add(helper[dirLeft]);
             }
-            res.Add(cp[left]);
-            return res;
+            helper[dirLeft] = i;
         }
 
-
-
-        public List<ChainPoint> checkMonotone(List<Point> p)
+        private void handleSplit(int i)
         {
-            List<ChainPoint> cp = new List<ChainPoint>();
-            int maxIdx = 0, minIdx = 0;
+            int dirLeft = T.DirectUpperAndLower(i).Value;
+            ll.Add(new Line(p[helper[dirLeft]], p[i]));
+            neighbors[Math.Max(helper[dirLeft], i)].Add(Math.Min(helper[dirLeft], i));
+            //neighbors[helper[dirLeft]].Add(i);
+            //neighbors[i].Add(helper[dirLeft]);
+            helper[dirLeft] = i;
+            T.Add(i);
+            helper[i] = i;
+        }
+        private void triangulateSubPol()
+        {
+            List<Line> curPoly = new List<Line>(); List<Line> resMT = new List<Line>(); List<Point> lP = new List<Point>();
+            List<Polygon> pol = new List<Polygon>();
+
+            List<bool> visited = new List<bool>();
+            LinkedList<int> ln = new LinkedList<int>();
             for (int i = 0; i < p.Count; ++i)
+            { ln.AddLast(i); visited.Add(false); }
+            for (LinkedListNode<int> cur = ln.First; cur != ln.Last.Next; cur = cur.Next)
             {
-                if (p[i].Y > p[maxIdx].Y)
-                    maxIdx = i;
-                if (p[i].Y < p[minIdx].Y)
-                    minIdx = i;
+                visited[cur.Value] = true;
+                for (int i = 0; i < neighbors[cur.Value].Count; ++i)
+                    if (visited[neighbors[cur.Value][i]])
+                    {
+                        int start = neighbors[cur.Value][i];
+                        int end = cur.Value;
+                        curPoly.Clear();
+                        LinkedListNode<int> saveCur = cur;
+                        for (int k = start; k < end; ++k)
+                        {
+                            curPoly.Add(new Line(p[k], p[(k + 1) % p.Count]));
+                            cur = cur.Previous;
+                            if (k > start)
+                                ln.Remove(cur.Next);
+                        }
+                        curPoly.Add(new Line(p[end], p[start]));
+                        resMT.Clear(); pol.Clear(); pol.Add(new Polygon(curPoly));
+                        new MonotoneTriangulation().Run(null, null, pol, ref lP, ref resMT, ref pol);
+                        ll.AddRange(resMT);
+                        cur = saveCur;
+                    }
+
             }
-            double prev = p[maxIdx].Y; cp.Add(new ChainPoint(p[maxIdx], 0));
-            for (int i = (maxIdx + 1) % p.Count; i != minIdx; prev = p[i].Y, cp.Add(new ChainPoint(p[i], -1)), i = (i + 1) % p.Count)
-                if (prev < p[i].Y)
-                    return null;
-            prev = p[minIdx].Y; cp.Add(new ChainPoint(p[minIdx], 0));
-            for (int i = (minIdx + 1) % p.Count; i != maxIdx; prev = p[i].Y, cp.Add(new ChainPoint(p[i], 1)), i = (i + 1) % p.Count)
-                if (prev > p[i].Y)
-                    return null;
-            return cp;
+            curPoly.Clear();
+            for (LinkedListNode<int> cur = ln.First; cur != ln.Last; cur = cur.Next)
+                curPoly.Add(new Line(p[cur.Value], p[cur.Next.Value]));
+            curPoly.Add(new Line(p[ln.Last.Value], p[ln.First.Value]));
+            resMT.Clear(); pol.Clear(); pol.Add(new Polygon(curPoly));
+            new MonotoneTriangulation().Run(null, null, pol, ref lP, ref resMT, ref pol);
+            ll.AddRange(resMT);
         }
+        private void triangulate()
+        {
+            List<Line> curPoly = new List<Line>(); List<Line> resMT = new List<Line>(); List<Point> lP = new List<Point>();
+            List<Polygon> pol = new List<Polygon>();
+            int start = 0;
+            int end = p.Count;
+            for (int i = start; i < end; ++i)
+            {
+                if (neighbors[i].Count > 0)
+                {
+                    for (int j = 0; j < neighbors[i].Count; ++j)
+                    {
+                        curPoly.Clear();
+                        int next = neighbors[i][j];
+                        while (((next + 1) % p.Count) != i)
+                        {
+                            if (next == end + 1)
+                                next = start;
+                            curPoly.Add(new Line(p[next], p[(next + 1) % p.Count]));
+                            next = (next + 1) % p.Count;
+                        }
+                        curPoly.Add(new Line(p[next], p[(next + 1) % p.Count]));
+                        curPoly.Add(new Line(p[i], p[neighbors[i][j]]));
+                        end = neighbors[i][j];
+                        start = i;
+
+                        resMT.Clear(); pol.Clear(); pol.Add(new Polygon(curPoly));
+                        new MonotoneTriangulation().Run(null, null, pol, ref lP, ref resMT, ref pol);
+                        ll.AddRange(resMT);
+                    }
+                }
+            }
+            curPoly.Clear();
+            for (int i = start; i < end; ++i)
+                curPoly.Add(new Line(p[i], p[i + 1]));
+            if ((end % p.Count) != start) //cusps found
+                curPoly.Add(new Line(p[end], p[start]));
+
+            resMT.Clear(); pol.Clear(); pol.Add(new Polygon(curPoly));
+            new MonotoneTriangulation().Run(null, null, pol, ref lP, ref resMT, ref pol);
+            ll.AddRange(resMT);
+        }
+
+        private int TCompX(int xx, int yy)
+        {
+            Point x = p[xx]; Point y = p[yy];
+            if (x.Equals(y))
+                return 0;
+            return x.X < y.X ? -1 : 1;
+        }
+
+        private bool interiorRight(int idx)
+        {
+            int prev = (idx + p.Count - 1) % p.Count;
+            int next = (idx + 1) % p.Count;
+            return (p[idx].Y < p[prev].Y && p[idx].Y > p[next].Y) ? true : false;
+        }
+        private int checkCase(int idx)
+        {
+            int pIdx = idx;
+            int prev = (pIdx + p.Count - 1) % p.Count;
+            int next = (pIdx + 1) % p.Count;
+            if (p[pIdx].Y > p[prev].Y && p[pIdx].Y > p[next].Y)
+                if (HelperMethods.CheckTurn(new Line(p[prev], p[pIdx]), p[next]) == Enums.TurnType.Left)
+                    return 0; //Start
+                else
+                    return 2; //Split
+            else if (p[pIdx].Y < p[prev].Y && p[pIdx].Y < p[next].Y)
+                if (HelperMethods.CheckTurn(new Line(p[prev], p[pIdx]), p[next]) == Enums.TurnType.Left)
+                    return 1; //End
+                else
+                    return 3; //Merge
+            return 4;
+        }
+
+        private int comp(MonoPoint x, MonoPoint y)
+        {
+            return x.p.Y > y.p.Y ? -1 : 1;
+        }
+
         public void checkPolygon(List<Point> p)
         {
             int minIdx = 0;
@@ -115,10 +259,9 @@ namespace CGAlgorithms.Algorithms.PolygonTriangulation
             if (HelperMethods.CheckTurn(new Line(p[prev], p[next]), p[minIdx]) == Enums.TurnType.Left)
                 p.Reverse();
         }
-
         public override string ToString()
         {
-            return "Monotone Triangulation";
+            return "Monotone Partitioning";
         }
     }
 }
